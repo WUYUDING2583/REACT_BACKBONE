884d2ec0b9329042c42bedd2779cbb9c9b655435
WUYUDING <e0533355@u.nus.edu>
2020-09-07 21:57:30 +0800

    'import_ant_design'

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,6 +7,7 @@ import {
   Route
 } from "react-router-dom";
 import Home from "./Home";
+import "./App.css";
 
 
 class App extends Component {
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,50 +1,51 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
+import { Button } from "antd";
 
 import {
-    bindActionCreators
-  } from "redux";
-  import {
-    connect
-  } from "react-redux";
-  import {
-    actions as appActions,
-    getError,
-    getGetRequestQuantity
-  } from "../../redux/modules/app";
+  bindActionCreators
+} from "redux";
+import {
+  connect
+} from "react-redux";
+import {
+  actions as appActions,
+  getError,
+  getGetRequestQuantity
+} from "../../redux/modules/app";
 
-class Home extends Component{
+class Home extends Component {
 
-    componentDidMount() {
-        // this.props.startGetRequest();
-        // setTimeout(() => this.props.finishGetRequest(), 5000);
-        this.props.test()
-            .then((result)=>{
-                console.log(result);
-                // alert(result)
-            })
-            .catch(err=>alert(err));
-      }
-    render(){
-        const { getRequestQuantity } = this.props;
-        return(
-            <div>{getRequestQuantity}</div>
-        )
-    }
+  componentDidMount() {
+    // this.props.startGetRequest();
+    // setTimeout(() => this.props.finishGetRequest(), 5000);
+    this.props.test()
+      .then((result) => {
+        console.log(result);
+        // alert(result)
+      })
+      .catch(err => alert(err));
+  }
+  render() {
+    const { getRequestQuantity } = this.props;
+    return (
+      <Button type="primary">{getRequestQuantity}</Button>
+    )
+  }
 }
 
 
 const mapStateToProps = (state, props) => {
-    return {
-      error: getError(state),
-      getRequestQuantity: getGetRequestQuantity(state),
-    }
+  return {
+    error: getError(state),
+    getRequestQuantity: getGetRequestQuantity(state),
   }
-  
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      ...bindActionCreators(appActions, dispatch),
-    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    ...bindActionCreators(appActions, dispatch),
   };
-  
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(Home);
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
